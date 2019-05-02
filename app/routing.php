1ccefca9660d2e682480addf57de508e059b65cd
Kr4v3n <fafachen37@gmail.com>
2019-05-02 17:06:46 +0200

    add admin routes

--- app/routing.php
+++ app/routing.php
@@ -10,6 +10,16 @@ $routes = [
         ['index', '/articles', 'GET'], //show index to users
         ['show', '/article/{id:\d+}', 'GET'], //show article to users
 
+    ],
+
+    'Admin' => [
+        ['showDashboard', '/admin/dashboard', 'GET'], //show dashboard for admin
+        ['adminShow', '/admin/article/{id:\d+}', 'GET'],//show article for admin
+        ['add', '/admin/createArticle', ['GET', 'POST']], // add an article admin
+        ['edit', '/admin/article/edit/{id:\d+}', ['GET', 'POST']], //modify an article admin
+        ['delete', '/admin/article/delete/{id:\d+}', 'GET'], // delete an article admin
+        ['indexAdmin', '/admin/articles', 'GET'], //show index for admin
+        ['logAdmin', '/admin/logAdmin', ['GET', 'POST']]
     ]
 
 ];
