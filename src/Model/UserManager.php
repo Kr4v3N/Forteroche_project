46f953984a2c39b4d851f01883a11679d2771512
Kr4v3n <fafachen37@gmail.com>
2019-05-03 17:19:14 +0200

    add functions usermanager

---
+++ src/Model/UserManager.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Model;
+
+/**
+ * Class UserManager
+ *
+ * @package \Model
+ */
+
+class UserManager extends AbstractManager
+{
+    const TABLE = 'user';
+    public function __construct(\PDO $pdo)
+    {
+        parent::__construct(self::TABLE, $pdo);
+    }
+
+
+    public function selectAllUsers(): array
+    {
+        return $this->pdo->query('SELECT * FROM user ORDER BY lastname', \PDO::FETCH_CLASS, $this->className)->fetchAll();
+    }
+
+    public function userDelete(int $id): int
+    {
+        $statement = $this->pdo->prepare("DELETE FROM user WHERE id=:id");
+        $statement->bindValue('id', $id, \PDO::PARAM_INT);
+        $statement->execute();
+        return header('Location: ' . $_SERVER['HTTP_REFERER']);
+    }
+
+}
