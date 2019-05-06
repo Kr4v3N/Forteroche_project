38d235095cd11ad3a2a5c93aaf90cf9e3f7b5dd0
Kr4v3n <fafachen37@gmail.com>
2019-05-06 17:03:59 +0200

    add hard BD, password bcrypt

---
+++ app/register.php
@@ -0,0 +1,9 @@
+<?php
+
+// Add the first admin hard bd, password bcrypt
+
+$pdo = new PDO('mysql:host=localhost;dbname=forteroche', 'root', 'root');
+
+$statement = $pdo->prepare("insert into user (firstname, lastname, email, pass, registered, status) VALUES ('admin', 'admin', 'super@root.com', :password, NOW(), 'admin' )");
+$statement->bindValue('password', password_hash('password', PASSWORD_DEFAULT));
+$statement->execute();
