feca0b2e3d7c6f353afe503b6c7edc08d703c38c
Kr4v3n <fafachen37@gmail.com>
2019-05-10 13:06:30 +0200

    add new redirection when a user not logged in try to comment

--- src/Controller/AdminCommentController.php
+++ src/Controller/AdminCommentController.php
@@ -12,25 +12,25 @@ use Model\Comment;
  */
 class AdminCommentController extends AbstractController
 {
-    // TODO add construct for user's connection
-
+// TODO add construct for user's connection
     public function add(int $articleId)
     {
         $errorConnexion ='';
+
         if (isset($_SESSION['user'])) {
             $CommentManager = new AdminCommentManager($this->getPdo());
             $comment = new Comment();
             $comment->setContent($_POST['content']);
             $comment->setArticleId($articleId);
-    // TODO modify user value with session start
+// TODO modify user value with session start
             $comment->setUserId($_SESSION['user']['id']);
             $id = $CommentManager->insert($comment);
             header('Location: /article/' . $articleId);
         }else{
             $errorConnexion = 'Vous devez être connecté pour commenter cet article.';
-            return $this->twig->render('Article/logToComment.html.twig', ['errorConnexion' => $errorConnexion]);
-            // renvoyer sur la page précédente après connexion
-//            $retour =
+            $retour = $_SERVER['HTTP_REFERER'];
+            return $this->twig->render('Article/logToComment.html.twig', ['errorConnexion' => $errorConnexion, 'retour' => $retour]);
+            // TODO renvoyer sur la page précédente après connexion
         }
     }
 
