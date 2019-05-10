5fb005dd2fde497bfd032e5c63891fd480447bfc
Kr4v3n <fafachen37@gmail.com>
2019-05-10 13:59:27 +0200

    change redirection routing

--- app/routing.php
+++ app/routing.php
@@ -15,12 +15,15 @@ $routes = [
     'Admin' => [
         ['showDashboard', '/admin/dashboard', 'GET'], //show dashboard for admin
         ['adminShow', '/admin/article/{id:\d+}', 'GET'], //show article for admin
-        ['add', '/admin/createArticle', ['GET', 'POST']], // add an article admin
+        ['add', '/admin/createArticle', ['GET', 'POST']], //add an article admin
         ['edit', '/admin/article/edit/{id:\d+}', ['GET', 'POST']], //modify an article admin
-        ['delete', '/admin/article/delete/{id:\d+}', 'GET'], // delete an article admin
+        ['delete', '/admin/article/delete/{id:\d+}', 'GET'], //delete an article admin
         ['indexAdmin', '/admin/articles', 'GET'], //show index for admin
-        ['logAdmin', '/admin/logAdmin', ['GET', 'POST']],
-        ['logout', '/admin/logout', 'GET'], //logout
+        ['logAdmin', '/admin/logAdmin', ['GET', 'POST']], //login admin
+        ['logout', '/admin/logout', 'GET'], //logout admin
+        ['userShow', '/admin/user/{id:\d+}', 'GET'], //show user for admin
+        ['usersIndex', '/admin/users', 'GET'], //show index users for admin
+        ['userDelete', '/admin/user/delete/{id:\d+}', 'GET'], //delete user admin
     ],
 
     'AdminComment' => [
@@ -30,12 +33,9 @@ $routes = [
     ],
 
     'User' => [
-        ['userShow', '/admin/user/{id:\d+}', 'GET'], //show user for admin
-        ['usersIndex', '/admin/users', 'GET'], //show index users for admin
-        ['userDelete', '/admin/user/delete/{id:\d+}', 'GET'], //delete user admin
         ['addUser', '/admin/user/createUser', ['GET', 'POST']],
-        ['suscribeUser', '/register', ['GET','POST']], // Register page
-        ['logUser', '/login', ['GET','POST']], // Login page
+        ['suscribeUser', '/register', ['GET','POST']], //register page
+        ['logUser', '/login', ['GET','POST']], //login page
 
     ]
 
