b165d10547f7a106bfb8bb69dd98d767c5eb59f7
Kr4v3n <fafachen37@gmail.com>
2019-05-11 05:03:16 +0200

    session flash fixed

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function($){
 
-    var alert = $('#alert');
+    let alert = $('#alert');
+
     if(alert.length > 0){
         alert.hide().slideDown(800).delay(2000).slideUp();
     }
