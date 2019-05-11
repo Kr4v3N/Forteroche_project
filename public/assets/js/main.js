4bb7d411dfde2215e5cbdd7c80305d5f01143f18
Kr4v3n <fafachen37@gmail.com>
2019-05-11 02:57:12 +0200

    Popup "you are logged in" for admin

---
+++ public/assets/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function($){
+
+    var alert = $('#alert');
+    if(alert.length > 0){
+        alert.hide().slideDown(800).delay(2000).slideUp();
+    }
+
+});
--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -24,14 +24,16 @@ class AdminController extends AbstractController
     }
 
     public function showDashboard() {
-        $article = "home";
+        $article = 'home';
         $countArticle = new ArticleManager($this->getPdo());            // connexion au pdo de l'article manager
         $numberArticles = $countArticle->count();                       // comptage du nombre d'article
         $countUsers = new UserManager($this->getPdo());                 // idem mais pour les utilisateurs
         $numberUsers = $countUsers->count();
         $countComment = new AdminCommentManager($this->getPdo());       // idem pour les commentaires
         $numberComments = $countComment->count();
-        return $this->twig->render('Admin/admin_dashboard.html.twig', ["active" => $article, "user" => $_SESSION['admin'], 'totalArticles' => $numberArticles, 'totalUsers' => $numberUsers, 'totalComments' => $numberComments]);
+        return $this->twig->render('Admin/admin_dashboard.html.twig', ['active' => $article, 'user' => $_SESSION['admin'],
+            'totalArticles' => $numberArticles, 'totalUsers' => $numberUsers,
+            'totalComments' => $numberComments, 'session' => $_SESSION]);
     }
 
 
